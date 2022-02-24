4cb1944729559519efe4010aa861b685278e5e14
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-02-24 04:07:35 -0500

    added generator.h
    added generator.h

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <coroutine>
 #include <iostream>
 #include <optional>
 #include <vector>
+#include "generator.h"
 
 
 bool CheckIfPrime(std::vector<long long>& primes, long long n)
@@ -17,100 +17,6 @@ bool CheckIfPrime(std::vector<long long>& primes, long long n)
     return isPrime;
 }
 
-template<std::movable T>
-class Generator {
-public:
-    struct promise_type {
-        Generator<T> get_return_object() {
-            return Generator{ Handle::from_promise(*this) };
-        }
-        static auto initial_suspend() noexcept {
-            return std::suspend_always{};
-        }
-        static auto final_suspend() noexcept {
-            return std::suspend_always{};
-        }
-        auto yield_value(T value) noexcept {
-            current_value = std::move(value);
-            return std::suspend_always{};
-        }
-        // Disallow co_await in generator coroutines.
-        void await_transform() = delete;
-        [[noreturn]]
-        static void unhandled_exception() {
-            throw;
-        }
-        // Added to supress compiler warnings
-        void return_void() noexcept {}
-        std::optional<T> current_value;
-    };
-
-    using Handle = std::coroutine_handle<promise_type>;
-
-    explicit Generator(const Handle coroutine) :
-        m_coroutine{ coroutine }
-    {}
-
-    Generator() = default;
-    ~Generator() {
-        if (m_coroutine) {
-            m_coroutine.destroy();
-        }
-    }
-
-    Generator(const Generator&) = delete;
-    Generator& operator=(const Generator&) = delete;
-
-    Generator(Generator&& other) noexcept :
-        m_coroutine{ other.m_coroutine }
-    {
-        other.m_coroutine = {};
-    }
-    Generator& operator=(Generator&& other) noexcept {
-        if (this != &other) {
-            if (m_coroutine) {
-                m_coroutine.destroy();
-            }
-            m_coroutine = other.m_coroutine;
-            other.m_coroutine = {};
-        }
-        return *this;
-    }
-
-    // Range-based for loop support.
-    class Iter {
-    public:
-        void operator++() {
-            m_coroutine.resume();
-        }
-        const T& operator*() const {
-            return *m_coroutine.promise().current_value;
-        }
-        bool operator==(std::default_sentinel_t) const {
-            return !m_coroutine || m_coroutine.done();
-        }
-
-        explicit Iter(const Handle coroutine) :
-            m_coroutine{ coroutine }
-        {}
-
-    private:
-        Handle m_coroutine;
-    };
-
-    Iter begin() {
-        if (m_coroutine) {
-            m_coroutine.resume();
-        }
-        return Iter{ m_coroutine };
-    }
-    auto end() {
-        return std::default_sentinel_t{};
-    }
-
-private:
-    Handle m_coroutine;
-};
 
 Generator<long long> GetInfinitePrimes() {
     std::vector<long long> primes;
