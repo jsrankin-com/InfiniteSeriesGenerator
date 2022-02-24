f42146fb2cc7d19a27b6072685c399715c28e4cf
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-02-24 04:22:36 -0500

    Code Cleanup
    Added Fibonacci test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+#include <coroutine>
 #include <iostream>
 #include <optional>
 #include <vector>
 #include "generator.h"
 
 
-bool CheckIfPrime(std::vector<long long>& primes, long long n)
+bool isPrime(std::vector<long long>& primes, long long n)
 {
     bool isPrime = true;
 
@@ -17,33 +18,95 @@ bool CheckIfPrime(std::vector<long long>& primes, long long n)
     return isPrime;
 }
 
-
 Generator<long long> GetInfinitePrimes() {
     std::vector<long long> primes;
     primes.push_back(2);
     co_yield 2;
     for (long long i = 3;; i += 2) {
-        if (CheckIfPrime(primes, i)) {
+        if (isPrime(primes, i)) {
             co_yield i;
             primes.push_back(i);
         }
     }
 }
 
-int main() {
-    auto gen = GetInfinitePrimes();
+int charToInt(char c)
+{
+    return c - '0';
+}
+char intToChar(int i)
+{
+    return i + '0';
+}
+std::string infiniteSum(std::string low, std::string high)
+{
+    std::vector<char> vec;
 
-    for (const auto i : gen) {
+    size_t lowLength = low.length();
+    size_t highLength = high.length();
+    size_t diff = highLength - lowLength;
+
+    int carry = 0;
+
+    for (int i = lowLength - 1; 0 <= i; --i)
+    {
+
+        int sum = charToInt(low[i]) +
+            charToInt(high[i + diff]) +
+            carry;
+        vec.emplace_back(intToChar(sum % 10));
+        carry = sum / 10;
+    }
+
+    for (int i = diff - 1; 0 <= i; --i)
+    {
+        int sum = (charToInt(high[i]) + carry);
+        vec.emplace_back(intToChar(sum % 10));
+        carry = sum / 10;
+    }
+
+    if (carry)
+        vec.emplace_back(intToChar(carry));
+
+    std::reverse(vec.begin(), vec.end());
+    
+    return std::string(vec.begin(), vec.end());
+}
+
+Generator<std::string> GetInfiniteFibonacci() {
+    co_yield "0";
+    co_yield "1";
+    co_yield "1";
+    std::string low = "1";
+    std::string high = "1";
+    std::string sum = "2";
+
+    for (;;) {
+        co_yield sum;
+        low = high;
+        high = sum;
+        sum = infiniteSum(low, high);
+    }
+}
+
+int main() {
+
+    std::cout << "Primes under 10:" << std::endl;
+    for (const auto i : GetInfinitePrimes()) {
+        if (i > 10)
+            break;
         std::cout << i << std::endl;
     }
 
-}
-/*
-References:
-https://en.cppreference.com/w/cpp/coroutine/coroutine_handle
+    std::cout << "First 10 Fibonacci numbers:" << std::endl;
+    long long count = 1;
+    for (const auto i : GetInfiniteFibonacci()) {
+        if (10 == count)
+            break;
+        std::cout << i << std::endl;
+        ++count;
+    }
 
-Author:
-	Jeffrey S. Rankin
-	http://jsrankin.com
-	admin@jsrankin.com
-*/
+    system("pause");
+    return 0;
+}
